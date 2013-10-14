7d374007294d980de05d4541169b0ab495307f1e
Nick Woll <nwoll27@gmail.com>
2013-10-14 14:27:48 -0400

    Created (empty) php and css files for theme

---
+++ nickwolldotcom/header.php
@@ -0,0 +1,20 @@
+ <?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package NickWoll-Dot-Com_Themes
+ * @subpackage NickWoll-Dot-Com
+ * @since NickWoll-Dot-Com 0.1
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
